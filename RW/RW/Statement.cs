fa8778aa97415cdf496778fcc41932a5f6ce6d35
Morgothus <106013514+Morgothus@users.noreply.github.com>
2022-05-26 21:34:52 +0200

    Add files via upload

---
+++ RW/RW/Statement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RW
+{
+    public interface Always
+    {
+        int[] condition { get; }
+    }
+
+    public interface Noninertial
+    {
+        int fluent { get; }
+    }
+
+    public interface Causes
+    {
+        int agent { get; } //id agenta
+        int action { get; } //id akcji
+        int[] condition { get; } //tablica wartosci fluentow w warunku: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+        int[] effect { get; } //tablica wartosci fluentow w efekcie akcji: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+    }
+
+    public interface TypicallyCauses
+    {
+        int agent { get; } //id agenta
+        int action { get; } //id akcji
+        int[] condition { get; } //tablica wartosci fluentow w warunku: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+        int[] effect { get; } //tablica wartosci fluentow w efekcie akcji: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+    }
+
+    public interface Releases
+    {
+        int agent { get; } //id agenta
+        int action { get; } //id akcji
+        int[] condition { get; } //tablica wartosci fluentow w warunku: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+        int[] effect { get; } //tablica wartosci fluentow w efekcie akcji: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+    }
+
+    public interface TypicallyReleases
+    {
+        int agent { get; } //id agenta
+        int action { get; } //id akcji
+        int[] condition { get; } //tablica wartosci fluentow w warunku: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+        int[] effect { get; } //tablica wartosci fluentow w efekcie akcji: 1 = prawda, 0 = falsz, -1 = bez znaczenia
+    }
+
+    public interface Initially
+    {
+        int[] condition { get; }
+    }
+
+    public interface After
+    {
+        List<(int agent, int action)> activity { get; }
+        int[] effect { get; }
+    }
+
+    public interface TypicallyAfter
+    {
+        List<(int agent, int action)> activity { get; }
+        int[] effect { get; }
+    }
+
+    public interface ObservableAfter
+    {
+        List<(int agent, int action)> activity { get; }
+        int[] effect { get; }
+    }
+
+    // dalej syntaktyka kwerend
+
+    // public interface Query_ExecutableAlways
+    // public interface Query_ExecutableEver
+    // public interface Query_AccessibleAlways
+    // public interface Query_AccessibleEver
+    // public interface Query_InvolvedAlways
+    // public interface Query_InvolvedEver
+}
