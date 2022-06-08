e4878a4ecc60b4bda63168271e37f5623e21ac4c
Bartek <bartek77@live.com>
2022-06-08 07:04:48 +0200

    Add queries examples for queries handling testing

---
+++ RWProgram/Tests_Programs.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RWProgram.Classes;
+using Action = RWProgram.Classes.Action;
+
+namespace RWProgram
+{
+    public static class Tests_Programs
+    {
+        public static List<(Action action, Actor actor)> Test1a
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test1b
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test1c
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "Anything", Index = 1000 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test2a
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test2b
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Jim", Index = 1 })
+                };
+        }
+        }
+
+        public static List<(Action action, Actor actor)> Test2c
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "ɛ", Index = 2 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test3a
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "SPIN", Index = 2 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test3b
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "SPIN", Index = 2 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test3c
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Anyone", Index = 1000 }),
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Anyone", Index = 1000 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test4a
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "INSERT_CARD", Index = 0 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test4b
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "INSERT_CARD", Index = 0 }, new Actor { Name = "ɛ", Index = 1 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test4c
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "Anything", Index = 1000 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test5a
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 }),
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test5b
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "SHOOT", Index = 1 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+
+        public static List<(Action action, Actor actor)> Test5c
+        {
+            get
+            {
+                return new List<(Action action, Actor actor)>
+                {
+                    (new Action { Name = "LOAD", Index = 0 }, new Actor { Name = "Bill", Index = 0 })
+                };
+            }
+        }
+    }
+}
---
+++ RWProgram/Tests_Queries.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RWProgram.Classes;
+using Action = RWProgram.Classes.Action;
+
+namespace RWProgram
+{
+    public static class Tests_Queries
+    {
+        public static AlwaysAccesibleYFromPi Test1a
+        {
+            get
+            {
+                return new AlwaysAccesibleYFromPi
+                {
+                    Gamma = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new NegatedFluent { Name = "alive", Original = new Fluent { Name = "alive", Index = 1 } }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+                        }
+                    },
+                    Pi = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new Fluent { Name = "alive", Index = 1 },
+                            new NegatedFluent { Name = "loaded", Original = new Fluent { Name = "loaded", Index = 0 } }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+                            new And()
+                        }
+                    }
+                };
+            }
+        }
+
+        public static AlwaysWInvolved Test1b
+        {
+            get
+            {
+                return new AlwaysWInvolved
+                {
+                    W = new Actor { Name = "Jim", Index = 1 }
+                };
+            }
+        }
+
+        public static EverWInvolved Test1c
+        {
+            get
+            {
+                return new EverWInvolved
+                {
+                    W = new Actor { Name = "Bill", Index = 0 }
+                };
+            }
+        }
+
+        public static EverAccesibleYFromPi Test2a
+        {
+            get
+            {
+                return new EverAccesibleYFromPi
+                {
+                    Gamma = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new NegatedFluent { Name = "alive", Original = new Fluent { Name = "alive", Index = 1 } }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+
+                        }
+                    },
+                    Pi = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new Fluent { Name = "alive", Index = 1 }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+
+                        }
+                    }
+                };
+            }
+        }
+
+        public static AlwaysExecutable Test2b
+        {
+            get
+            {
+                return new AlwaysExecutable
+                {
+                };
+            }
+        }
+
+        public static EverExecutable Test2c
+        {
+            get
+            {
+                return new EverExecutable
+                {
+                };
+            }
+        }
+
+        public static AlwaysExecutable Test3a
+        {
+            get
+            {
+                return new AlwaysExecutable
+                {
+                };
+            }
+        }
+
+        public static AlwaysAccesibleYFromPi Test3b
+        {
+            get
+            {
+                return new AlwaysAccesibleYFromPi
+                {
+                    Gamma = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new Fluent { Name = "loaded", Index = 0 }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+                        }
+                    },
+                    Pi = new State
+                    {
+                        Fluents = new List<Fluent>
+                        {
+                            new NegatedFluent { Name = "loaded", Original = new Fluent { Name = "loaded", Index = 0 } }
+                        },
+                        Operators = new List<LogicOperator>
+                        {
+                        }
+                    }
+                };
+            }
+        }
+
+        public static EverExecutable Test3c
+        {
+            get
+            {
+                return new EverExecutable
+                {
+                };
+            }
+        }
+
+        public static AlwaysWInvolved Test4a
+        {
+            get
+            {
+                return new AlwaysWInvolved
+                {
+                    W = new Actor { Name = "ɛ", Index = 1 }
+                };
+            }
+        }
+
+        public static EverExecutable Test4b
+        {
+            get
+            {
+                return new EverExecutable
+                {
+                };
+            }
+        }
+
+        public static EverExecutable Test4c
+        {
+            get
+            {
+                return new EverExecutable
+                {
+                };
+            }
+        }
+
+        public static EverWInvolved Test5a
+        {
+            get
+            {
+                return new EverWInvolved
+                {
+                    W = new Actor { Name = "ɛ", Index = 1 }
+                };
+            }
+        }
+
+        public static AlwaysExecutable Test5b
+        {
+            get
+            {
+                return new AlwaysExecutable
+                {
+                };
+            }
+        }
+
+        public static AlwaysWInvolved Test5c
+        {
+            get
+            {
+                return new AlwaysWInvolved
+                {
+                    W = new Actor { Name = "ɛ", Index = 1 }
+                };
+            }
+        }
+    }
+}
